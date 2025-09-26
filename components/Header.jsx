facb9fa2574d8aa08beb4c856f54b0f4dc16060b
Tonny_G <gesoranthony@gmail.com>
2025-09-26 21:58:34 +0300

    udated nav

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,15 +4,28 @@ import hornbill from "../public/Hornbill.png";
 
 const NavBar = () => {
   return (
-    <nav className="flex flex-row justify-between items-center px-[10px] navBar md:h-[10%] h-[7%]">
-      <div className="">
+    <nav className="flex flex-row justify-between items-center  navBar ">
+      <div className="w-fit flex justify-center items-center  ">
         <Image
+          alt="hornbill icon"
           src={hornbill}
           className="md:h-[60px] md:w-[60px] h-[8vh] w-[8vh]"
         />
+        <span className="text-[16px]">
+          Hi, <br /> <span className="text-[#9c3e03] text-[18px]">Anthony</span>
+        </span>
       </div>
-      <div className="bg-amber-900">center</div>
-      <div className="bg-amber-300">right</div>
+
+      {/* desktop navigation */}
+      <div className="w-full flex justify-evenly">
+        <div className="bg-amber-900">center</div>
+        <div className="bg-amber-300">right</div>
+      </div>
+      {/* mobile navigation */}
+
+      <div className="w-full flex justify-evenly">
+        <Link href="#">Home</Link>
+      </div>
     </nav>
   );
 };
